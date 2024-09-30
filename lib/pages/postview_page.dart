6743fbdf5c9b85f32d83df2d7581769a2218c5c2
PuriSipSip <Dettanawiwat@gmail.com>
2024-09-30 16:14:14 +0700

    for display detailed post information

---
+++ lib/pages/postview_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/models/posts_model.dart';
+
+class PostViewPage extends StatelessWidget {
+  final PostsModel post;
+
+  const PostViewPage({super.key, required this.post});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
