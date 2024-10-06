052fede0370cf40495b278f22ae354337c7907af
PuriSipSip <Dettanawiwat@gmail.com>
2024-10-06 16:47:11 +0700

    Create postview page

--- lib/pages/postview_page.dart
+++ lib/pages/postview_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/models/posts_model.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 class PostViewPage extends StatelessWidget {
   final PostsModel post;
@@ -10,56 +11,149 @@ class PostViewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          post.catname,
-          style:
-              const TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.lightBlue,
+          iconTheme: const IconThemeData(color: Colors.white),
         ),
-        centerTitle: true,
-        backgroundColor: Colors.lightBlue,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              width: 400, // กำหนดความกว้างของภาพ
-              height: 300, // กำหนดความสูงของภาพ
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(6),
-              ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(6),
-                child: CachedNetworkImage(
-                  imageUrl: post.imageURL,
-                  placeholder: (context, url) =>
-                      const Center(child: CircularProgressIndicator()),
-                  errorWidget: (context, url, error) =>
-                      const Icon(Icons.error, size: 50),
-                  fit: BoxFit.cover, // ปรับให้รูปภาพเต็มขนาดของ Container
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // Avatar , username
+                Row(
+                  children: [
+                    CircleAvatar(
+                      backgroundImage:
+                          CachedNetworkImageProvider(post.photoURL),
+                      backgroundColor: Colors.grey[300],
+                      radius: 18,
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Text(
+                      post.username,
+                      style: const TextStyle(
+                          color: Colors.lightBlue,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    const Spacer(),
+                    IconButton(
+                        icon: Icon(Icons.more_horiz, color: Colors.grey[500]),
+                        onPressed: () {})
+                  ],
                 ),
-              ),
-            ),
 
-            // description
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(height: 4),
-                  Text(
-                    post.description,
-                    style: const TextStyle(
-                        fontWeight: FontWeight.bold, fontSize: 16),
+                const Divider(color: Colors.white),
+
+                // imageURL
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 300,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(6),
                   ),
-                ],
-              ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(6),
+                    child: CachedNetworkImage(
+                      imageUrl: post.imageURL,
+                      placeholder: (context, url) =>
+                          const CircularProgressIndicator(),
+                      errorWidget: (context, url, error) => const Icon(
+                        Icons.error,
+                        size: 50,
+                      ),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+
+                // catname , iconlike , comment
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        post.catname,
+                        style: const TextStyle(
+                            color: Colors.lightBlue,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Row(
+                        children: [
+                          IconButton(
+                            icon: const Icon(Icons.favorite_border),
+                            onPressed: () {
+                              // funtion likepost
+                            },
+                          ),
+                          const SizedBox(
+                            width: 0.5,
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.chat_bubble_outline),
+                            onPressed: () {
+                              // funtion comment
+                            },
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  children: [
+                    Text(
+                      timeago.format(post.timestamp.toDate()),
+                      style: const TextStyle(color: Colors.grey, fontSize: 12),
+                    ),
+                    const SizedBox(
+                      width: 6,
+                    ),
+                    const Icon(Icons.favorite, color: Colors.red, size: 16),
+                    Text(
+                      '${post.likesBy.length} likes',
+                      style: const TextStyle(fontSize: 12),
+                    )
+                  ],
+                ),
+                const Divider(),
+
+                // username , description
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4.0),
+                  child: RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: '${post.username}    ', // Username
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14,
+                            color: Colors.lightBlue,
+                          ),
+                        ),
+                        TextSpan(
+                          text: post.description, // Description
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey[800],
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
