b39990ed58bc8ce02c1570cc4d7b48238d69cad2
PuriSipSip <Dettanawiwat@gmail.com>
2024-10-07 18:51:50 +0700

    Create Users Model

---
+++ lib/models/users_model.dart
@@ -0,0 +1,34 @@
+class UsersModel {
+  String email;
+  String username;
+  String photoURL;
+  String bio;
+
+  UsersModel({
+    required this.email,
+    required this.username,
+    required this.photoURL,
+    this.bio = 'love cats!', // Default value for bio
+  });
+
+  // Convert model to Map for Firestore
+  Map<String, dynamic> toMap() {
+    return {
+      'email': email,
+      'username': username,
+      'photoURL': photoURL,
+      'bio': bio,
+    };
+  }
+
+  // Factory method to create model from Firestore data
+  factory UsersModel.fromMap(Map<String, dynamic> data) {
+    return UsersModel(
+      email: data['email'] ?? 'Unknown email',
+      username: data['username'] ?? 'Unknown username',
+      photoURL: data['photoURL'] ??
+          'https://firebasestorage.googleapis.com/v0/b/socialcat-5e8af.appspot.com/o/dinosauravatar.png?alt=media&token=b3bbad75-a5ce-4866-a27b-447620405054',
+      bio: data['bio'] ?? 'love cats!',
+    );
+  }
+}
