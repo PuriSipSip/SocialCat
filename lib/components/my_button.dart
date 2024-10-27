925a833a887190b30f015348d50bb1fc56bb8db7
PuriSipSip <Dettanawiwat@gmail.com>
2024-10-27 17:52:47 +0700

    Update logo , bg color

--- lib/components/my_button.dart
+++ lib/components/my_button.dart
@@ -19,7 +19,7 @@ class MyButton extends StatelessWidget {
         padding: const EdgeInsets.all(25),
         margin: const EdgeInsets.symmetric(horizontal: 25),
         decoration: BoxDecoration(
-          color: Colors.black,
+          color: Colors.lightBlue,
           borderRadius: BorderRadius.circular(8),
         ),
         child: Center(
